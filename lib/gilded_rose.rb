720583d325a2c505e717faa3603d4dcc748e812e
Dalton Mann <daltonjamann@gmail.com>
2024-02-28 12:49:29 -0600

    Completed refactoring gilded rose

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,61 +8,41 @@ class GildedRose
   end
 
   def tick
-    if name == 'normal'
+    case name
+    when 'Normal Item'
       return normal_tick
-    end
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
+    when 'Aged Brie'
+      return brie_tick
+    when 'Sulfras, Hand of Ragnaros'
+      return sulfras_tick
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      return backstage_tick
     end
   end
 
   def normal_tick
-    if @days_remaining > 0
-      @quality -= 1
-    end
-    if @days_remaining <= 0
-      @quality -= 2
-    end
     @days_remaining -= 1
+    return if @quality == 0
+    @quality -= 1
+    @quality -= 1 if @days_remaining <= 0
+  end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    @quality +=1
+    @quality +=1 if @days_remaining <= 0 && @quality < 50
+  end
+
+  def sulfras_tick
+  end
+
+  def backstage_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    return @quality = 0 if @days_remaining < 0
+    @quality += 1
+    @quality += 1 if @days_remaining <10
+    @quality += 1 if @days_remaining < 5
   end
 end
